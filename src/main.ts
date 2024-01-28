22c0bfdaa9ec9c7fb44447463c154fa9fa261adf
Pedro X. <pxpm88@gmail.com>
2024-01-28 14:01:17 +0000

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
+import * as github from '@actions/github'
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -7,18 +7,21 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token = core.getInput('repo-token', { required: true })
+    const tag = core.getInput('tag', { required: true })
+    const sha =
+      core.getInput('commit-sha', { required: false }) || github.context.sha
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const client = github.getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`tagging #${sha} with tag ${tag}`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await client.rest.git.createRef({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: `refs/tags/${tag}`,
+      sha
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
